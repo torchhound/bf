4dabb2f2da9b0b64ce1d1f8f31f082e92a68c91c
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2017-02-09 15:32:21 -0500

    Major revisions and cleanup, added further error checking, tests currently run without error but never finish or generate output

--- interpreter.py
+++ interpreter.py
@@ -2,37 +2,81 @@ import sys
 
 array = [0] * 1000
 
-ptr = 0
+previousWhile = 0
+
+dataPtr = 0
+
+prgPtr = 0
 
 def parse(program):
 	global array
-	global ptr
+	global dataPtr
+	global prgPtr
 	while(program):
 		def goto():
+			global array
+			global dataPtr
+			global prgPtr
 			try:
-				eval(program[0])
+				if dataPtr < 0:
+					dataPtr = 0
+					print("Pointer has been reset to zero because it was negative.")
+				if prgPtr < 0:
+					print("Fatal Error: Instruction Pointer less than zero")
+					quit()
+				print("Eval")
+				eval(program[prgPtr], program)
 			except(IndexError):
+				print("Extending")
 				array.extend([0] * 1000)
 				goto()
 			except(ValueError):
 				pass
 		goto()
-		program = program[1:]
+		if prgPtr == len(program):
+			break
+		prgPtr += 1
 
+def increment(x):
+	return x + 1
 
-def eval(x):
+def decrement(x):
+	return x - 1
+
+def beginLoop():
 	global array
-	global ptr
+	global dataPtr
+	global prgPtr
+	global previousWhile
+	if array[dataPtr] == 0: 
+		for x, item in enumerate(program): 
+			if item == "]": 
+				previousWhile = prgPtr
+				prgPtr = x + 1
+
+def closeLoop():
+	global array
+	global dataPtr
+	global prgPtr
+	global previousWhile
+	if array[dataPtr] != 0: 
+		prgPtr = previousWhile;
+
+def eval(x, program):
+	global array
+	global dataPtr
+	global prgPtr
+	global previousWhile
 	command = {
-		">" : lambda _: ptr += 1,
-		"<" : lambda _: ptr -= 1,
-		"+" : lambda _: array[ptr] += 1,
-		"-" : lambda _: array[ptr] -= 1,  
-		"." : lambda _: print(array[ptr]),
-		"," : lambda _: sys.stdin.read(),
-		"[" : lambda _: , #make while loop
-		"]" : lambda _: , #close while loop
-	}.get(x, ValueError) #pass instead of ValueError? [x]() instead of get? and use a try except block instead of default in .get()?
+		">" : increment(dataPtr),
+		"<" : decrement(dataPtr),
+		"+" : increment(array[dataPtr]),
+		"-" : decrement(array[dataPtr]),  
+		"." : print(array[dataPtr]),
+		"," : sys.stdin.read(1),
+		"[" : beginLoop(), 
+		"]" : closeLoop(), 
+	}.get(x, ValueError) 
 
 	return command()
 
--- tests.py
+++ tests.py
@@ -4,7 +4,9 @@ import interpreter
 class InterpreterTests(unittest.TestCase):
 	"""Tests for interpreter.py"""
 
-	
+	def testInterpreterHelloWorld(self):
+		"""Checks output of hello world"""
+		self.assertTrue(interpreter.parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.") == "Hello World!")
 
 if __name__ == "__main__":
 	unittest.main()
